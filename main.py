0824b52c6313362d95d35e533f46bde1c6d82007
Yy <yyned2501@vip.qq.com>
2025-06-13 07:18:14 +0000

    添加异步支持和改进代码格式，优化主程序启动逻辑

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+# 前置加速
+import sys
+
+if sys.platform != "win32":
+    import uvloop
+
+    uvloop.install()
+
 # 标准库
 import asyncio
 
-#自定义模块
+# 自定义模块
 from app import start_app
 
 
 if __name__ == "__main__":
     asyncio.run(start_app())
-    print('Connected..', flush=True)
+    print("Connected..", flush=True)
