b906311e438b44a08fe476a62a59b5fdbd242f48
Yy <yyned2501@vip.qq.com>
2025-06-25 04:09:47 +0000

    修复bash函数

--- libs/async_bash.py
+++ libs/async_bash.py
@@ -1,14 +1,19 @@
 import asyncio
+from libs.log import logger
+import traceback
 
 
 async def bash(command):
     command_list = command.split(" ")
-    proc = await asyncio.create_subprocess_exec(
-        *command_list,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await proc.communicate()
-    if proc.returncode != 0:
-        raise RuntimeError(f"bash {command} failed: {stderr.decode().strip()}")
-    return stdout.decode().strip()
+    try:
+        proc = await asyncio.create_subprocess_exec(
+            *command_list,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout = "\n".join(map(lambda x: x.decode().strip(), await proc.communicate()))
+        if proc.returncode != 0:
+            raise RuntimeError(f"bash {command} failed: {stdout}")
+        return stdout
+    except Exception as e:
+        logger.error(traceback.format_exc())
