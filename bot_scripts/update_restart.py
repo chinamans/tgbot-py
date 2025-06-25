ba031536b12f081a090309c20a2ba14cf1138892
Yy <yyned2501@vip.qq.com>
2025-06-25 02:38:09 +0000

    移除本地变更

--- bot_scripts/update_restart.py
+++ bot_scripts/update_restart.py
@@ -70,6 +70,7 @@ tags = None
 @Client.on_message(filters.chat(MY_TGID) & filters.command("update"))
 async def update_tg_bot(client: Client, message: Message):
     global tags
+    await bash("git reset --hard")
     await bash("git fetch --prune --prune-tags origin")
     tags = ["origin/main"] + (await bash("git tag --sort=-creatordate")).split("\n")
     tags = tags[:6]
