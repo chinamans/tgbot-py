86bd9b044f868f648bfaa420159732d4cca23a9f
Yy <yyned2501@vip.qq.com>
2025-06-26 02:27:37 +0000

    bug fix BE模式修改下注策略，不跟几连，采用连败

--- bot_scripts/universal/basic.py
+++ bot_scripts/universal/basic.py
@@ -22,25 +22,17 @@ MESSAGE = "基础设置"
 
 class Basic(Method):
     auto_restart = (auto(), "自动重启", "toggle")
-    # autochangename = (auto(), "自动修改报时昵称", "toggle")
-    # autofire = (auto(), "朱雀自动释放技能", "toggle")
-    # lotterysw = (auto(), "小菜自动抽奖", "toggle")
 
 
 inline_button = InlineButton(SITE_NAME, ACTION, MESSAGE)
 
 
-def main_keyboard():
+async def main_keyboard():
     return InlineKeyboardMarkup(
         [
             [
-                inline_button.create_button(Basic.auto_restart),
-                # inline_button.create_button(Basic.autochangename),
+                await inline_button.create_button(Basic.auto_restart),
             ],
-            # [
-            #     inline_button.create_button(Basic.autofire),
-            #     inline_button.create_button(Basic.lotterysw),
-            # ],
             [inline_button.close_button()],
         ]
     )
@@ -51,7 +43,9 @@ inline_button.set_main_keyboard(main_keyboard)
 
 @Client.on_message(filters.chat(MY_TGID) & filters.command(ACTION))
 async def basic_set(_, message: Message):
-    await message.reply(inline_button.main_message(), reply_markup=main_keyboard())
+    await message.reply(
+        inline_button.main_message(), reply_markup=await main_keyboard()
+    )
 
 
 @Client.on_callback_query(CallbackDataFromFilter(ACTION))
