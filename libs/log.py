eb5d3b1ca889a9cc8113d2b4aa27ae0155f57099
Yy <yyned2501@vip.qq.com>
2025-07-07 12:15:51 +0800

    优化日志格式化和日志级别设置

--- libs/log.py
+++ libs/log.py
@@ -10,15 +10,18 @@ from logging.handlers import RotatingFileHandler
 import pytz
 
 
-
 # 可选：东八区时间格式
 class CSTFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
-        time_utc8 = datetime.fromtimestamp(record.created, pytz.timezone('Asia/Shanghai'))
-        return time_utc8.strftime(datefmt or '%Y-%m-%d %H:%M:%S(%Z)')
+        time_utc8 = datetime.fromtimestamp(
+            record.created, pytz.timezone("Asia/Shanghai")
+        )
+        return time_utc8.strftime(datefmt or "%Y-%m-%d %H:%M:%S(%Z)")
+
+
 formatter = CSTFormatter("[%(levelname)s] %(asctime)s - %(filename)s - %(message)s")
-logger = logging.getLogger('main')
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
 
 # 防止重复添加 handler
 # 创建 logs 目录
@@ -28,11 +31,13 @@ log_path.mkdir(parents=True, exist_ok=True)
 # 设置日志文件路径
 log_file = log_path / "Mytgbot.log"
 
-if not logger.handlers:    
-    file_handler = RotatingFileHandler(log_file, maxBytes=10 * 1024 * 1024, backupCount=10, encoding="utf-8")
+if not logger.handlers:
+    file_handler = RotatingFileHandler(
+        log_file, maxBytes=10 * 1024 * 1024, backupCount=10, encoding="utf-8"
+    )
     file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)    
-    utf8_stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    logger.addHandler(file_handler)
+    utf8_stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
     console_handler = logging.StreamHandler(utf8_stdout)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
